8521aeca9394527eb65f4ee4a0c8bf28d69881f3
Andy Brunner <andy@cloudxs.ch>
2021-05-18 07:22:40 +0200

    components dir created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import React from 'react'
 import logo from './logo.svg';
+import Header from './components/Header'
+import Item from './components/Item'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      newItemValue: '',
+      items: [
+        {text:"First todo", done:false},
+        {text:"Second todo", done:true}
+      ]
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggleTodo = this.toggleTodo.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({newItemValue: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let items = this.state.items.slice()
+
+    items.push({
+      text: this.state.newItemValue,
+      done:false
+    })
+
+    this.setState({
+      newItemValue: '',
+      items: items
+    })
+  }
+
+  toggleTodo(index) {
+    let items = this.state.items.slice()
+    let item = items[index]
+    item.done = !item.done
+
+    this.setState({items: items})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="New todo..." value={this.state.newItemValue} onChange={this.handleChange} />
+          <input type="submit" value="Add Item" />
+        </form>
+
+        <ol>
+          {this.state.items.map((item,index) => (
+            <Item key={index} clickHandler={()=> this.toggleTodo(index)} text={item.text} done={item.done} />
+          ))}
+        </ol>
+      </div>
+    );
+    }
 }
 
 export default App;
